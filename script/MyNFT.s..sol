69af876892b4be442efe771bdd61fa3522c3cbbf
Eunum56 <msiaffu123@gmail.com>
2025-04-20 23:50:08 +0530

    Update MyNFTScript

--- script/MyNFT.s..sol
+++ script/MyNFT.s..sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import {Script, console} from "forge-std/Script.sol";
 import {MyNFT} from "../src/MyNFT.sol";
 
-contract CounterScript is Script {
+contract MyNFTScript is Script {
     MyNFT public myNFT;
 
     function setUp() public {}
